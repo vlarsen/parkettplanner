d4e9066c92e8355726518b131453ac3610bb8af9
Vidar Larsen <vlarsen@telenordigital.com>
2018-01-11 12:44:00 +0100

    consistent naming and code style cleanup

--- index.js
+++ index.js
@@ -2,108 +2,101 @@
 
 var argv = require('minimist')(process.argv.slice(2),
     {
-        string: [ "rowstart", "rowrestart" ],
+        string: [ "startlen" ],
         default: {
             bx: 220,
             by: 19,
-            miny: 40
+            minbx: 40,
+            minby: 5
         }
     });
 
 const series_label = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
-const bord_bredde = argv.by;
-const bord_lengde = argv.bx;
-const min_rest = argv.miny;
+const board_width = argv.by;
+const board_length = argv.bx;
+const min_rest_length = argv.minbx;
+const min_board_width = argv.minby;
 const room_width = argv.rx;
 const room_height = argv.ry;
-const series_offset = argv.rowstart.split("/").map(x => parseInt(x))
+const series_lengths = argv.startlen.split("/").map(l => parseInt(l));
 
-
-console.log("/boardwidth " + bord_bredde + " def");
-console.log("/boardlength " + bord_lengde + " def");
+console.log("/boardwidth " + board_width + " def");
+console.log("/boardlength " + board_length + " def");
 
 console.log("/showrowlabel " + (argv.showlabel ? "true" : "false") + " def");
 console.log("/showlenstr " + (argv.showlen ? "true" : "false") + " def");
 
-function fraction_length_from(x) {
+function rest_length_starting_at(x) {
     // start at x
     var sum_len = x;
     // add whole boards
-    sum_len += Math.floor((room_width - sum_len) / bord_lengde) * bord_lengde;
+    sum_len += Math.floor((room_width - sum_len) / board_length) * board_length;
     // return what is left
     return room_width - sum_len;
 }
-var fraction_length = fraction_length_from(0);
+var global_rest_length = rest_length_starting_at(0);
 // console.log("fraction: " + fraction_length);
 
 // check end_piece constraint on initial offsets
-series_offset.forEach(x => {
-    var end_piece = fraction_length_from(x);
-    if (end_piece < min_rest) {
+series_lengths.forEach(function(x) {
+    var end_piece = rest_length_starting_at(x);
+    if (end_piece < min_rest_length) {
         console.error("WARNING: start length " + x + " gives too short end piece: " + end_piece);
-        process.exit(0);
+        process.exit(1);
     }
-})
+});
 
 function fraction_iter_max_len_with_valid_endpiece(startx) {
     // start searching at startx
     var candidate = startx;
-    // in steps equal to the global fraction length
+    // in steps equal to the global rest length
 
-    // go up to maximum bord_lengde
-    while (candidate < bord_lengde) {
-        candidate += fraction_length;
+    // go up to maximum board_length
+    while (candidate <= board_length - global_rest_length) {
+        candidate += global_rest_length;
     }
     // go down if the end piece is too short
-    while (fraction_length_from(candidate) < min_rest) {
-        candidate -= fraction_length;
+    while (rest_length_starting_at(candidate) < min_rest_length) {
+        candidate -= global_rest_length;
     }
     return candidate;
 }
 
-const series_restart = series_offset.map(x => fraction_iter_max_len_with_valid_endpiece(x))
-// console.log("rowstart: " + series_offset);
-// console.log("restart : " + series_restart);
+const series_restart_lenghts = series_lengths.map(fraction_iter_max_len_with_valid_endpiece);
 
-var row = 1;
+var row = 0;
 var series_idx = 0;
 
-while (row * bord_bredde < room_height) {
-    var output_line = "";
-	// console.log("row " + row);
+while (row * board_width < room_height) {
+    row++;
 
-    var this_offset = series_offset[series_idx];
+    const this_length = series_lengths[series_idx];
 
-    // console.log("using series " + series_label[series_idx] + " with offset " + this_offset);
+    var row_length = this_length;
+    var row_offsets = [ this_length ];
 
-    var row_length = this_offset;
-    var row_offsets = [ this_offset ];
-
-    row_length += bord_lengde;
+    row_length += board_length;
     while (row_length < room_width) {
-        row_offsets.push(bord_lengde);
-        row_length += bord_lengde;
+        row_offsets.push(board_length);
+        row_length += board_length;
     }
-    var row_rest = row_length - room_width;
-    var last_offset = bord_lengde - row_rest;
-    row_offsets.push(last_offset);
+    var row_rest_length = row_length - room_width;
+    var last_board_length = board_length - row_rest_length;
+    row_offsets.push(last_board_length);
 
-    // console.log("results: " + row_offsets);
+    // Add * to the label if you need to throw away the remaining piece
+    console.log("[" + row_offsets.join(" ") +"] (" + series_label[series_idx] +
+        ((row_rest_length < min_rest_length) ? "*" : "")+ ") row");
 
-    output_line += "[";
-    row_offsets.forEach(function (value) {
-        output_line += " " + value; })
-    output_line += " ] (" + series_label[series_idx] + ") row";
-    console.log(output_line);
+    series_lengths[series_idx] = (row_rest_length >= min_rest_length) ? row_rest_length : series_restart_lenghts[series_idx];
+    series_idx = (series_idx + 1) % series_lengths.length;
+}
 
-    if (row_rest >= min_rest) {
-        series_offset[series_idx] = row_rest;
-    } else {
-        series_offset[series_idx] = series_restart[series_idx];
-    }
-
-    series_idx = (series_idx + 1) % series_offset.length;
-
-	row++;
+var all_boards_total_width = row * board_width;
+var extra_width = all_boards_total_width - room_height;
+var last_row_actual_width = board_width - extra_width;
+if (last_row_actual_width < min_board_width) {
+    console.error("WARNING: Last row is too narrow: " + last_row_actual_width);
+    process.exit(1);
 }
