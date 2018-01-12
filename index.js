0bb04954f2f053e11134e3b55d3e7b63e94579d3
Vidar Larsen <vlarsen@telenordigital.com>
2018-01-12 11:38:08 +0100

    bump language level and use string templates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var argv = require('yargs-parser')(process.argv.slice(2),
+const argv = require('yargs-parser')(process.argv.slice(2),
     {
         alias: {
             startlen: ['s'],
@@ -42,27 +42,26 @@ console.log("/showlenstr " + (argv.showlen ? "true" : "false") + " def");
 
 function rest_length_starting_at(x) {
     // start at x
-    var sum_len = x;
+    let sum_len = x;
     // add whole boards
     sum_len += Math.floor((room_width - sum_len) / board_length) * board_length;
     // return what is left
     return room_width - sum_len;
 }
-var global_rest_length = rest_length_starting_at(0);
-// console.log("fraction: " + fraction_length);
+const global_rest_length = rest_length_starting_at(0);
 
 // check end_piece constraint on initial offsets
 series_lengths.forEach(function(x) {
-    var end_piece = rest_length_starting_at(x);
+    const end_piece = rest_length_starting_at(x);
     if (end_piece < min_rest_length) {
-        console.error("WARNING: start length " + x + " gives too short end piece: " + end_piece);
+        console.error("WARNING: start length ${x} gives too short end piece: ${end_piece}");
         process.exit(1);
     }
 });
 
 function fraction_iter_max_len_with_valid_endpiece(startx) {
     // start searching at startx
-    var candidate = startx;
+    let candidate = startx;
     // in steps equal to the global rest length
 
     // go up to maximum board_length
@@ -78,38 +77,37 @@ function fraction_iter_max_len_with_valid_endpiece(startx) {
 
 const series_restart_lenghts = series_lengths.map(fraction_iter_max_len_with_valid_endpiece);
 
-var row = 0;
-var series_idx = 0;
+let row = 0;
+let series_idx = 0;
 
 while (row * board_width < room_height) {
     row++;
 
     const this_length = series_lengths[series_idx];
 
-    var row_length = this_length;
-    var row_offsets = [ this_length ];
+    let row_length = this_length;
+    const row_offsets = [this_length];
 
     row_length += board_length;
     while (row_length < room_width) {
         row_offsets.push(board_length);
         row_length += board_length;
     }
-    var row_rest_length = row_length - room_width;
-    var last_board_length = board_length - row_rest_length;
+    const row_rest_length = row_length - room_width;
+    const last_board_length = board_length - row_rest_length;
     row_offsets.push(last_board_length);
 
     // Add * to the label if you need to throw away the remaining piece
-    console.log("[" + row_offsets.join(" ") +"] (" + series_label[series_idx] +
-        ((row_rest_length < min_rest_length) ? "*" : "")+ ") row");
+    console.log(`[ ${row_offsets.join(" ")} ] (${series_label[series_idx]}${((row_rest_length < min_rest_length) ? "*" : "")}) row`);
 
     series_lengths[series_idx] = (row_rest_length >= min_rest_length) ? row_rest_length : series_restart_lenghts[series_idx];
     series_idx = (series_idx + 1) % series_lengths.length;
 }
 
-var all_boards_total_width = row * board_width;
-var extra_width = all_boards_total_width - room_height;
-var last_row_actual_width = board_width - extra_width;
+const all_boards_total_width = row * board_width;
+const extra_width = all_boards_total_width - room_height;
+const last_row_actual_width = board_width - extra_width;
 if (last_row_actual_width < min_board_width) {
-    console.error("WARNING: Last row is too narrow: " + last_row_actual_width);
+    console.error(`WARNING: Last row is too narrow: ${last_row_actual_width}`);
     process.exit(1);
 }
